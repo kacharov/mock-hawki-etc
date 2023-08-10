7f4512f3e6b53ab3f542cca54b2704ba3de79155
Nikolay Kacharov <kacharov@aip.de>
2023-08-10 17:24:01 +0200

    Clarify the example.py file

--- example.py
+++ example.py
@@ -9,16 +9,22 @@ Created on Wed Aug  9 11:58:54 2023
 from hawki_etc import hawki_etc
 from astropy import units as u
 
+# Initiate the HAWK-I ETC
 etc = hawki_etc()
 
+# Compute the total exposure time given a set of parameters and a target SNR
 texp = etc.snr2texp(mag=22.5*u.mag, band='Ks', snr=5, airmass=1.2,
                     seeing=0.8*u.arcsec, dit=10*u.s)
 
+# Compute the SNR given a set of parameters and a total exposure time
 snr = etc.texp2snr(mag=22.5, band='Ks', texp=3600*u.s,
                    airmass=1.2, seeing=0.8*u.arcsec, dit=10*u.s)
 
+# Print the results
 print()
 print('Texp = ', texp)
 print('SNR = ', snr)
 print()
+
+# Reveal all attributes of the etc class
 print(dir(etc))
